29f73dbbabd8811330a06ecf84d9c8bbe409c1b6
JAK343 <97551997+JAK343@users.noreply.github.com>
2022-10-07 17:58:44 +0100

    Small punctuation errors corrected

--- inspireMeGenerator.js
+++ inspireMeGenerator.js
@@ -12,9 +12,9 @@ function returnQuoteOne(quotesArray) {
 
 let numberOne = returnQuoteOne([`You've got to get up every morning with determination if you're going to go to bed with satisfaction. — George Lorimer.`,
  `Education is the most powerful weapon which you can use to change the world. — Nelson Mandela.`, 
- `The most difficult thing is the decision to act, the rest is merely tenacity. —Amelia Earhart.`,
+ `The most difficult thing is the decision to act, the rest is merely tenacity. — Amelia Earhart.`,
   `You’ll find that education is just about the only thing lying around loose in this world, and it’s about the only thing a fellow can have as much of as he’s
-   willing to haul away. —John Graham.`]);
+   willing to haul away. — John Graham.`]);
 
 
 
@@ -31,7 +31,7 @@ function returnQuoteTwo(secondQuotesArray) {
 let numberTwo = returnQuoteTwo([`5.	Take the attitude of a student, never be too big to ask questions, never know too much to learn something new. — Augustine Og Mandino.`,
  `The greatest discovery of my generation is that a human being can alter his life by altering his attitudes. — William James.`,
   `One of the differences between some successful and unsuccessful people is that one group is full of doers, while the other is full of wishers. — Edmond Mbiaka.`,
-   `I’d rather regret the things I’ve done than regret the things I haven’t done. —Lucille Ball.`]);
+   `I’d rather regret the things I’ve done than regret the things I haven’t done. — Lucille Ball.`]);
 
 // Get message three //
 
@@ -43,7 +43,7 @@ function returnQuoteThree(thirdQuotesArray) {
 
 // Declaring with long list of quotes
 
-let numberThree = returnQuoteThree([`You cannot plow a field by turning it over in your mind. To begin, begin. ―Gordon B. Hinckley.`,
+let numberThree = returnQuoteThree([`You cannot plow a field by turning it over in your mind. To begin, begin. ― Gordon B. Hinckley.`,
  `When you arise in the morning think of what a privilege it is to be alive, to think, to enjoy, to love… – Marcus Aurelius.`,
  `Be miserable. Or motivate yourself. Whatever has to be done, it's always your choice. – Wayne Dyer.`,
 `Your Monday morning thoughts set the tone for your whole week. See yourself getting stronger, and living a fulfilling, happier & healthier life. – Germany Kent.`]);
