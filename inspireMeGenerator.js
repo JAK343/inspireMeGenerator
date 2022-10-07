50ce185685b489454f3102b84a3728a7391bd30b
JAK343 <97551997+JAK343@users.noreply.github.com>
2022-10-07 17:19:12 +0100

    Let i/j/k functionality for random number added

--- inspireMeGenerator.js
+++ inspireMeGenerator.js
@@ -1,15 +1,22 @@
 // inspireMeGenerator //
 
 // Get message one //
-function returnQuoteOne() {
+function returnQuoteOne(quotesArray) {
+    let i = Math.floor(Math.random()* 4)
+    console.log(i);
     let numberOne = 'Test 1';
     return numberOne;
 }
+
+// Attempting to make function with for loop work
+
 let numberOne = returnQuoteOne();
 
 // Get message two //
 
-function returnQuoteTwo() {
+function returnQuoteTwo(secondQuotesArray) {
+    let j = Math.floor(Math.random()* 4)
+    console.log(j);
     let numberTwo = 'Test 2';
     return numberTwo;
 }
@@ -17,7 +24,9 @@ let numberTwo = returnQuoteTwo();
 
 // Get message three //
 
-function returnQuoteThree() {
+function returnQuoteThree(thirdQuotesArray) {
+    let k = Math.floor(Math.random()* 4)
+    console.log(k);
     let numberThree = 'Test 3';
     return numberThree;
 }
