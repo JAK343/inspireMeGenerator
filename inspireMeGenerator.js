ef4fa243ee920303b998f35b053a38f4b4ee21a3
JAK343 <97551997+JAK343@users.noreply.github.com>
2022-10-07 17:13:36 +0100

    Returning 3 test quotes as expected

--- inspireMeGenerator.js
+++ inspireMeGenerator.js
@@ -1,11 +1,32 @@
 // inspireMeGenerator //
 
 // Get message one //
+function returnQuoteOne() {
+    let numberOne = 'Test 1';
+    return numberOne;
+}
+let numberOne = returnQuoteOne();
 
 // Get message two //
 
+function returnQuoteTwo() {
+    let numberTwo = 'Test 2';
+    return numberTwo;
+}
+let numberTwo = returnQuoteTwo();
+
 // Get message three //
 
-// Return message //
+function returnQuoteThree() {
+    let numberThree = 'Test 3';
+    return numberThree;
+}
+let numberThree = returnQuoteThree();
 
+// Return message //
+function printAllQuotes() {
+    console.log(numberOne, numberTwo, numberThree);
+}
+
+printAllQuotes();
 
